ab2c0bb50b98076c4c2008bf285d31f4664d00ad
Marina42 <marina.sauca@gmail.com>
2016-10-10 15:40:34 +0200

    Initial Commit

---
+++ HalaCore/HalaCore.h
@@ -0,0 +1,19 @@
+//
+//  HalaCore.h
+//  HalaCore
+//
+//  Created by Marina Sauca on 10/10/2016.
+//  Copyright © 2016 Hala. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for HalaCore.
+FOUNDATION_EXPORT double HalaCoreVersionNumber;
+
+//! Project version string for HalaCore.
+FOUNDATION_EXPORT const unsigned char HalaCoreVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <HalaCore/PublicHeader.h>
+
+
