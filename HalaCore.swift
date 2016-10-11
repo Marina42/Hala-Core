39dc1f17f050d62d30879d9cc4b5a6c24117f966
Marina42 <marina.sauca@gmail.com>
2016-10-11 08:53:48 +0200

    submodule commit

---
+++ HalaCore.swift
@@ -0,0 +1,18 @@
+//
+//  HalaCore.swift
+//  HalaCore
+//
+//  Created by Marina Sauca on 10/10/2016.
+//  Copyright © 2016 Hala. All rights reserved.
+//
+
+import Foundation
+
+public class HalaCore{
+    public static let halaCore = HalaCore()
+    private init(){}
+    public func test(){
+        print("FrameWork works")
+    }
+}
+
